8b4a6f5905f4a18363bbad032b1fd1a3f7854e20
YashChavan999 <yaash.chavan@gmail.com>
2026-01-28 18:40:12 -0500

    Increased wireframe geometry scaling

--- src/assets/shaders/geom_vert.glsl
+++ src/assets/shaders/geom_vert.glsl
@@ -19,7 +19,7 @@ void main() {
 
     // If it's a wireframe pass, scale up the geometry to avoid z-fighting with solid pass
     if (color_override.a > 0.0) {
-        pos *= 1.001;
+        pos *= 1.01;
     }
 
     gl_Position = view_proj * model * vec4(pos, 1.0);
