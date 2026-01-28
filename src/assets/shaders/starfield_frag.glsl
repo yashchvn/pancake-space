96e01ebedee29259bc6c50b4bb85ca81c6d3346a
YashChavan999 <yaash.chavan@gmail.com>
2026-01-28 18:40:41 -0500

    Updated starfield shaders

--- src/assets/shaders/starfield_frag.glsl
+++ src/assets/shaders/starfield_frag.glsl
@@ -1,5 +1,4 @@
 #version 100
-precision mediump float;
 
 void main() {
     gl_FragColor = vec4(1.0);
--- src/assets/shaders/starfield_vert.glsl
+++ src/assets/shaders/starfield_vert.glsl
@@ -5,6 +5,6 @@ attribute vec3 star_pos;
 uniform mat4 view_proj;
 
 void main() {
-    gl_PointSize = 20.0;
+    gl_PointSize = 1.0;
     gl_Position = view_proj * vec4(star_pos, 1.0);
 }
